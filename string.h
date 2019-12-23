ef82db5a08f20729c586eb26f37e38ed3213d0fd
Mark Seminatore <mseminatore@users.noreply.github.com>
2019-12-23 12:11:05 -0800

    added more functions

--- string.h
+++ string.h
@@ -19,9 +19,14 @@ int strcoll(const char *str1, const char *str2);
 int strncmp(const char *str1, const char *str2, size_t num);
 size_t strxfrm(char *destination, const char *source, size_t num);
 
+void *memchr(void *ptr, int value, size_t num);
 const char *strchr(const char *str, int character);
-//char *strchr(char *str, int character);
 size_t strcspn(const char *str1, const char *str2);
+char *strpbrk(char *str1, const char *str2);
+char *strrchr(char *str, int character);
+size_t strspn(const char *str1, const char *str2);
+char *strstr(char *str1, const char *str2);
+char *strtok(char *str, const char *delimiters);
 
 void *memset (void *ptr, int value, size_t num);
 char *strerror(int errnum);
