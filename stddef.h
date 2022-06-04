685fb1e68b8e51177f443fa7de5ae617634b916b
Mark Seminatore <mseminatore@users.noreply.github.com>
2022-06-04 11:24:40 -0700

    adjusted size_t defn

--- stddef.h
+++ stddef.h
@@ -1,6 +1,12 @@
 #ifndef __STDDEF_H
 #define __STDDEF_H
 
-typedef unsigned long size_t;
+#ifndef size_t
+    #if defined(_WIN32)
+        typedef unsigned int size_t;
+    #else
+        typedef unsigned long size_t;
+    #endif
+#endif
 
 #endif // __STDDEF_H
