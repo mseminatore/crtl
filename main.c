0c7dc1cf9627a12d509bb4bb64920fee7056fe28
Mark Seminatore <mseminatore@users.noreply.github.com>
2022-06-09 16:01:07 -0700

    updated and added tests

--- main.c
+++ main.c
@@ -5,8 +5,25 @@
 //
 void test_strlen()
 {
-	assert(strlen("Hello") == 4);
+	puts("Testing strlen()\n");
+
+	assert(strlen("Hello") == 5);
 	assert(strlen("") == 0);
+}
+
+//
+void test_memcpy()
+{
+	puts("Testing memcmp()\n");
+
+}
+
+//
+void test_strcmp()
+{
+	puts("Testing strcmp()\n");
+
+	assert(0 == strcmp("same", "same"));
 }
 
 //
@@ -15,4 +32,5 @@ int main(int argc, char *argv[])
 	puts("Starting tests\n");
 
 	test_strlen();
+	test_strcmp();
 }
