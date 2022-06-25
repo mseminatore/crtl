30597bc8694f238fc95a17d984a674a8f7f3c0ad
Mark Seminatore <mseminatore@users.noreply.github.com>
2022-06-25 11:52:25 -0700

    start implementing IO functions

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # CRTL
 
-This is intended to be a basic and portable implementation of an ANSI C runtime library. This is a fun
-and educational exercise. I also hope to use this with a C compiler that I've written for microcontrollers.
+This is intended to be a basic and portable implementation of an ANSI C 
+runtime library. The is not to provide 100% of the CRTL, rather the
+most commonly used functions.
 
+This is primarily an educational exercise. Development and testing
+will be mainly on Windows and MacOS, with Linux support added in the
+future.
+
+At some point I also hope to use this with a C compiler that I've written for microcontrollers.
--- stdio.c
+++ stdio.c
@@ -65,8 +65,58 @@ int puts(const char *str)
 }
 
 //
+// int open(const char *pathname, int flags)
+// {
+// 	return -1;
+// }
+
+// //
+// int close(int fd)
+// {
+// 	return syscall(SYS_close, fd);
+// }
+
+// //
+// size_t read(int d, void *buf, size_t count)
+// {
+// 	return syscall(SYS_read, buf, count);
+// }
+
+// //
+// int write(int fd, const void *buf, size_t count)
+// {
+// 	return syscall(SYS_write, buf, count);
+// }
+
+//
+FILE *fopen(const char *filename, const char *mode)
+{
+	assert(filename && mode);
+	if (!filename || ! mode)
+		return NULL;
+
+	return NULL;
+}
+
+//
+int fclose(FILE *stream)
+{
+	assert(stream);
+	if (!stream)
+		return EOF;
+
+	#if defined(_WIN32)
+	#else
+		int result = close(stream->fildes);
+		stream->fildes = -1;
+		return result;
+	#endif
+}
+
+//
 int printf(const char *format, ...)
 {
+	// TODO - replace this with full impl
 	puts(format);
 	return 0;
 }
